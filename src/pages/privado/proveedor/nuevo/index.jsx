f11c077914d5690fa6157ca01cac8b1056fb7e0c
Renato Gonzales <rent.gonz@gmail.com>
2021-12-05 00:58:28 -0500

    Se agrego lo de las distritos pe

--- src/pages/privado/proveedor/nuevo/index.jsx
+++ src/pages/privado/proveedor/nuevo/index.jsx
@@ -1,234 +1,239 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 //import { Guarda } from "./components/Guarda";
-import { Lista } from "../conponents/Lista";
-import "./nuevo.css";
+import { Lista } from '../conponents/Lista';
+import './nuevo.css';
 
 function PageProvNuevo() {
-  const distritos = [
-    {
-      title: "Lima",
-      value: "lim",
-    },
-    {
-      title: "Ayacucho",
-      value: "ayac",
-    },
-    {
-      title: "Cuzco",
-      value: "cus",
-    },
-  ];
+	// const distritos = [
+	// 	{
+	// 		title: 'Lima',
+	// 		value: 'lim',
+	// 	},
+	// 	{
+	// 		title: 'Ayacucho',
+	// 		value: 'ayac',
+	// 	},
+	// 	{
+	// 		title: 'Cuzco',
+	// 		value: 'cus',
+	// 	},
+	// ];
 
-  const [departamentos, setDepartamento] = useState([]);
-  const [provincias, setProvincia] = useState([]);
-  const [form, setForm] = useState({
-    codigo: "",
-    nombre: "",
-    ruc: "",
-    departamento: "",
-    provincia: "",
-    distrito: "",
-    direccion: "",
-  });
+	const [departamentos, setDepartamento] = useState([]);
+	const [response, setResponse] = useState([]);
+	const [provincias, setProvincia] = useState([]);
+	const [form, setForm] = useState({
+		codigo: '',
+		nombre: '',
+		ruc: '',
+		departamento: '',
+		provincia: '',
+		distrito: '',
+		direccion: '',
+	});
 
-  function saveProveedor(proveedor) {
-    axios
-      .post("http://localhost:4000/proveedor", proveedor)
-      .then((response) => {
-        alert("El proveedor se guardo correctamente");
-      })
-      .catch(() => {
-        alert("Por favor intentalo nuevamente");
-      });
-  }
+	function saveProveedor(proveedor) {
+		axios
+			.post('http://localhost:4000/proveedor', proveedor)
+			.then((response) => {
+				alert('El proveedor se guardo correctamente');
+			})
+			.catch(() => {
+				alert('Por favor intentalo nuevamente');
+			});
+	}
 
-  function getDepartamento() {
-    axios
-      .get("http://localhost:4000/departamentos")
-      .then((response) => {
-        setDepartamento(response.data);
-        setProvincia(response.data);
-        //console.log(response.data[0].provincias);
-        //console.log(response.data[0].provincias[1].nombre);
-      })
-      .catch((e) => {});
-  }
+	function getDepartamento() {
+		axios
+			.get('http://localhost:4000/departamentos')
+			.then((response) => {
+				setResponse(response.data);
+			})
+			.catch((e) => {});
+	}
 
-  useEffect(() => {
-    getDepartamento();
-    setDepartamento(
-      departamentos.filter((dep) => dep.nombre === form.departamento)
-    );
-    setProvincia(
-      departamentos.filter((prov) => prov.nombre === form.provincia)
-    );
-  }, [form.departamento, form.provincia]);
+	useEffect(() => {
+		getDepartamento();
+	}, []);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    saveProveedor(form);
-    setForm({
-      codigo: "",
-      nombre: "",
-      ruc: "",
-      departamento: "",
-      provincia: "",
-      distrito: "",
-      direccion: "",
-    });
-  }
+	useEffect(() => {
+		setDepartamento(
+			response.filter((dep) => {
+				return dep.nombre === form.departamento;
+			})
+		);
+		setProvincia(
+			departamentos[0]
+				? departamentos[0].provincias.filter(
+						(prov) => prov.nombre === form.provincia
+				  )
+				: []
+		);
+	}, [form.departamento, form.provincia, form.distrito]);
 
-  return (
-    <div className="nuevo">
-      <h1>Nuevo Proveedor</h1>
-      <hr />
+	function handleSubmit(e) {
+		e.preventDefault();
+		saveProveedor(form);
+		setForm({
+			codigo: '',
+			nombre: '',
+			ruc: '',
+			departamento: '',
+			provincia: '',
+			distrito: '',
+			direccion: '',
+		});
+	}
 
-      <div class="mb-4">
-        {/* <Guarda /> */}
-        <Lista />
-      </div>
+	return (
+		<div className='nuevo'>
+			<h1>Nuevo Proveedor</h1>
+			<hr />
 
-      <form onSubmit={handleSubmit}>
-        <div class="mb-3 col-6">
-          <label for="codigo" class="form-label">
-            Codigo
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="codigo"
-            placeholder="Id"
-            required
-            value={form.codigo}
-            onChange={(e) =>
-              setForm((state) => ({ ...state, codigo: e.target.value }))
-            }
-          />
-        </div>
-        <div class="mb-3 col-6">
-          <label for="nombreproveedor" class="form-label">
-            Nombre
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="nombreproveedor"
-            placeholder="Nombre Proveedor"
-            required
-            value={form.nombre}
-            onChange={(e) =>
-              setForm((state) => ({ ...state, nombre: e.target.value }))
-            }
-          />
-        </div>
-        <div class="mb-3 col-6">
-          <label for="ruc" class="form-label">
-            Ruc
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="ruc"
-            placeholder="Ruc"
-            required
-            value={form.ruc}
-            onChange={(e) =>
-              setForm((state) => ({ ...state, ruc: e.target.value }))
-            }
-          />
-        </div>
-        <div class="mb-3 col-6">
-          <label for="ubigeo" class="form-label">
-            Ubigeo
-          </label>
-          <div class="d-flex justify-content-between">
-            <select
-              required
-              class="form-select"
-              aria-label="Default select example"
-              value={form.departamento}
-              onChange={(e) => {
-                setForm((state) => {
-                  return {
-                    ...state,
-                    departamento: e.target.value,
-                  };
-                });
-              }}
-            >
-              <option selected>Departamento</option>
-              {departamentos.map((departamento) => {
-                return <option>{departamento.nombre}</option>;
-              })}
-            </select>
-            <select
-              class="form-select"
-              aria-label="Default select example"
-              value={form.provincia}
-              onChange={(e) => {
-                // setForm((state) => {
-                //   return {
-                //     ...state,
-                //     provincia: e.target.value,
-                //   };
-                // });
-                setProvincia(
-                  departamentos[0].provincias.filter(
-                    (prov) => prov.nombre === form.provincia
-                  )
-                );
-              }}
-            >
-              <option selected>Provincia</option>
-              {!!provincias.length &&
-                provincias.map((provincia) => (
-                  <option>{provincia.nombre}</option>
-                ))}
-              {/* {provincias.map((provincia) => (
+			<div className='mb-4'>
+				{/* <Guarda /> */}
+				<Lista />
+			</div>
+
+			<form onSubmit={handleSubmit}>
+				<div className='mb-3 col-6'>
+					<label for='codigo' class='form-label'>
+						Codigo
+					</label>
+					<input
+						type='text'
+						className='form-control'
+						id='codigo'
+						placeholder='Id'
+						required
+						value={form.codigo}
+						onChange={(e) =>
+							setForm((state) => ({ ...state, codigo: e.target.value }))
+						}
+					/>
+				</div>
+				<div class='mb-3 col-6'>
+					<label for='nombreproveedor' class='form-label'>
+						Nombre
+					</label>
+					<input
+						type='text'
+						className='form-control'
+						id='nombreproveedor'
+						placeholder='Nombre Proveedor'
+						required
+						value={form.nombre}
+						onChange={(e) =>
+							setForm((state) => ({ ...state, nombre: e.target.value }))
+						}
+					/>
+				</div>
+				<div class='mb-3 col-6'>
+					<label for='ruc' class='form-label'>
+						Ruc
+					</label>
+					<input
+						type='text'
+						className='form-control'
+						id='ruc'
+						placeholder='Ruc'
+						required
+						value={form.ruc}
+						onChange={(e) =>
+							setForm((state) => ({ ...state, ruc: e.target.value }))
+						}
+					/>
+				</div>
+				<div class='mb-3 col-6'>
+					<label for='ubigeo' class='form-label'>
+						Ubigeo
+					</label>
+					<div class='d-flex justify-content-between'>
+						<select
+							required
+							className='form-select'
+							aria-label='Default select example'
+							value={form.departamento}
+							onChange={(e) => {
+								setForm((state) => {
+									return {
+										...state,
+										departamento: e.target.value,
+									};
+								});
+							}}
+						>
+							<option selected>Departamento</option>
+							{response.map((departamento) => {
+								return (
+									<option key={departamento.id}>{departamento.nombre}</option>
+								);
+							})}
+						</select>
+						<select
+							className='form-select'
+							aria-label='Default select example'
+							value={form.provincia}
+							onChange={(e) => {
+								setForm((state) => {
+									return {
+										...state,
+										provincia: e.target.value,
+									};
+								});
+							}}
+						>
+							<option selected>Provincia</option>
+							{!!departamentos.length &&
+								departamentos[0].provincias.map((provincia) => (
+									<option key={provincia}>{provincia.nombre}</option>
+								))}
+							{/* {provincias.map((provincia) => (
                 <option>{provincia.title}</option>
               ))} */}
-            </select>
-            <select
-              class="form-select"
-              aria-label="Default select example"
-              value={form.distrito}
-              onChange={(e) => {
-                setForm((state) => {
-                  return {
-                    ...state,
-                    distrito: e.target.value,
-                  };
-                });
-              }}
-            >
-              <option selected>Distrito</option>
-              {distritos.map((distrito) => (
-                <option>{distrito.title}</option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div class="mb-3 col-6">
-          <label for="direccion" class="form-label">
-            Direccion
-          </label>
-          <textarea
-            class="form-control"
-            id="direccion"
-            rows="2"
-            value={form.direccion}
-            onChange={(e) =>
-              setForm((state) => ({ ...state, direccion: e.target.value }))
-            }
-          ></textarea>
-        </div>
-        <div>
-          <button className="btn btn-success">GUARDAR</button>
-        </div>
-      </form>
-    </div>
-  );
+						</select>
+						<select
+							className='form-select'
+							aria-label='Default select example'
+							value={form.distrito}
+							onChange={(e) => {
+								setForm((state) => {
+									return {
+										...state,
+										distrito: e.target.value,
+									};
+								});
+							}}
+						>
+							<option selected>Distrito</option>
+							{provincias.length &&
+								provincias[0].distritos.map((distrito) => (
+									<option key={distrito}>{distrito}</option>
+								))}
+						</select>
+					</div>
+				</div>
+				<div className='mb-3 col-6'>
+					<label for='direccion' className='form-label'>
+						Direccion
+					</label>
+					<textarea
+						class='form-control'
+						id='direccion'
+						rows='2'
+						value={form.direccion}
+						onChange={(e) =>
+							setForm((state) => ({ ...state, direccion: e.target.value }))
+						}
+					></textarea>
+				</div>
+				<div>
+					<button className='btn btn-success'>GUARDAR</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export { PageProvNuevo };
